593d35ef39423b60b0cfb5a5da9106042a92a7aa
Abewd <116496332+abewd@users.noreply.github.com>
2023-01-09 18:46:58 +1100

    added API as a text as opposed to using an environment

--- assets/script.js
+++ assets/script.js
@@ -18,84 +18,80 @@ function saveToLocal() {
 
   var userGoal = {
     name: userName,
-    goal: userGoal
-  }
+    goal: userGoal,
+  };
 
   localStorage.setItem("savedGoal", JSON.stringify(userGoal));
 
-  printGoal()
+  printGoal();
 }
 
 //Retrieve data from local storage, if no data, print no name and ... as goal on the page instead
 function printGoal() {
   var savedUserGoal = JSON.parse(localStorage.getItem("savedGoal")) || {
     name: "",
-    goal: "..."
-  }
+    goal: "...",
+  };
 
-  console.log(savedUserGoal)
+  console.log(savedUserGoal);
 
   var savedUser = document.querySelector("#savedUser");
-  savedUser.textContent = savedUserGoal.name
-  var savedGoal = document.querySelector("#savedGoal")
-  savedGoal.textContent = savedUserGoal.goal
-
-
+  savedUser.textContent = savedUserGoal.name;
+  var savedGoal = document.querySelector("#savedGoal");
+  savedGoal.textContent = savedUserGoal.goal;
 }
 
 submitBtn.addEventListener("click", function () {
   console.log("Saving!");
   saveToLocal();
   modalBox.style.display = "none";
-  getImg()
-  getQuote()
+  getImg();
+  getQuote();
 });
 
+printGoal();
 
-printGoal()
-
-var accessKey = process.env.ACCESS_KEY
-var getUrl = "https://api.unsplash.com/photos/random/?client_id=" + accessKey
-var randomImg = document.querySelector("#randomImg")
-var imgSource = document.querySelector("#imgLink")
-var imgCreator = document.querySelector("#creator")
-var lastImg = localStorage.getItem("lastImg")
-var randomQuote = document.querySelector("#quote")
-function getImg (){
-fetch(getUrl)
-  .then(function (response){
-    return response.json();
-  })
-  .then(function(data){
-    console.log(data)
-    randomImg.src = data.urls.regular
-    imgSource.setAttribute("href", data.links.html)
-    imgCreator.textContent = data.user.name
-    imgCreator.setAttribute("href", data.user.portfolio_url)
-    localStorage.setItem("lastImg", data.urls.regular)
-    
-  })
+var accessKey = "aoHR-Un2l_uxsLT6clT61cCEzrmim18upkshDbMvrPU";
+var getUrl = "https://api.unsplash.com/photos/random/?client_id=" + accessKey;
+var randomImg = document.querySelector("#randomImg");
+var imgSource = document.querySelector("#imgLink");
+var imgCreator = document.querySelector("#creator");
+var lastImg = localStorage.getItem("lastImg");
+var randomQuote = document.querySelector("#quote");
+function getImg() {
+  fetch(getUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      randomImg.src = data.urls.regular;
+      imgSource.setAttribute("href", data.links.html);
+      imgCreator.textContent = data.user.name;
+      imgCreator.setAttribute("href", data.user.portfolio_url);
+      localStorage.setItem("lastImg", data.urls.regular);
+    });
 }
 //retrieve img & quote saved in local storage so it appears on page even after refreshing
-randomImg.src = lastImg
-randomQuote.textContent = savedQuote
+randomImg.src = lastImg;
+savedQuote = randomQuote.textContent;
+// randomQuote.textContent = savedQuote;
 //fetch API for random quote
 function getQuote() {
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'X-RapidAPI-Key': '754b4e35d4msh554b1fa15fcc530p1b2c10jsncbfab6b043ef',
-      'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
-    }
+      "X-RapidAPI-Key": "754b4e35d4msh554b1fa15fcc530p1b2c10jsncbfab6b043ef",
+      "X-RapidAPI-Host": "quotes15.p.rapidapi.com",
+    },
   };
-  
-  fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
-    .then(response => response.json())
-    .then(function(data){
-      console.log(data)
-      randomQuote.textContent = "'" + data.content + "'"
-      localStorage.setItem("savedQuote", data.content)
-    })
-    .catch(err => console.error(err)); 
-}
 
+  fetch("https://quotes15.p.rapidapi.com/quotes/random/", options)
+    .then((response) => response.json())
+    .then(function (data) {
+      console.log(data);
+      randomQuote.textContent = "'" + data.content + "'";
+      localStorage.setItem("savedQuote", data.content);
+    })
+    .catch((err) => console.error(err));
+}
